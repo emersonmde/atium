136c796c02a045290621275d22b4e8a56714b4bf
emersonmde <emersonmde@protonmail.com>
2024-02-16 03:29:15 +0000

    Deploying to gh-pages from @ emersonmde/atium@4edfbaa503157ec8f67ea2c4cae079372b1b120d 🚀

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"atium":["",[["algebra",[],["add.rs","constant.rs","expression.rs","mod.rs","multiply.rs","parser.rs","variable.rs"]]],["lib.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["atium",["",[["algebra",[],["add.rs","constant.rs","expression.rs","mod.rs","multiply.rs","parser.rs","variable.rs"]]],["lib.rs"]]]\
+]'));
 createSrcSidebar();
